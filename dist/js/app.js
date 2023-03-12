374f0959686417e9a2df318b71094feeb0f5e9d3
Andrew-devcoder <andrey.kovpak.01@gmail.com>
2023-03-12 23:56:53 +0200

    input: hover and mask

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,8 +1,18 @@
-require("dotenv").config();
+// require('dotenv').config();
 
-console.log("Init!");
+console.log('Init!');
 
-const form = document.querySelector(".form__body");
-const telSelector = form.querySelector('input[type="tel"]');
-const inputMask = new Inputmask("+38 (999) 999-99-99");
-inputMask.mask(telSelector);
+// const form = document.querySelector(".form__body");
+// const telSelector = form.querySelector('input[type="tel"]');
+// const inputMask = new Inputmask("+38 (999) 999-99-99");
+// inputMask.mask(telSelector);
+
+// import Inputmask from 'inputmask';
+// var Inputmask = require('inputmask');
+
+const form = document.querySelector('form');
+const myInput = form.querySelector('input[type="tel"]');
+const inputMask = new Inputmask('+38 (999) 999-99-99');
+inputMask.mask(myInput);
+
+// Inputmask({ 'mask': '(999) 999-9999' }).mask(myInput);

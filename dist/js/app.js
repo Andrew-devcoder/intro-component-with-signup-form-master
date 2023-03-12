1996f661401d48b19f4233d78fa2e3dc02abc1de
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2023-03-12 19:03:59 +0200

    draft

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,6 +3,6 @@ require("dotenv").config();
 console.log("Init!");
 
 const form = document.querySelector(".form__body");
-const tel = form.querySelector('input[type="tel"]');
-const telMask = new Inputmask("+38 (999) 999 99 99");
-telMask.mask(tel);
+const telSelector = form.querySelector('input[type="tel"]');
+const inputMask = new Inputmask("+38 (999) 999-99-99");
+inputMask.mask(telSelector);

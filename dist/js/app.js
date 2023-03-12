e5974cc84a899fb969c574df7d091dbe000c9cb2
Andrew-devcoder <andrey.kovpak.01@gmail.com>
2023-03-13 00:11:46 +0200

    add js file

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,17 +2,97 @@
 
 console.log('Init!');
 
-// const form = document.querySelector(".form__body");
-// const telSelector = form.querySelector('input[type="tel"]');
-// const inputMask = new Inputmask("+38 (999) 999-99-99");
-// inputMask.mask(telSelector);
-
-// import Inputmask from 'inputmask';
-// var Inputmask = require('inputmask');
-
 const form = document.querySelector('form');
 const myInput = form.querySelector('input[type="tel"]');
 const inputMask = new Inputmask('+38 (999) 999-99-99');
 inputMask.mask(myInput);
 
-// Inputmask({ 'mask': '(999) 999-9999' }).mask(myInput);
+const validation = new JustValidate('.form');
+
+validation
+    .addField('#name', [
+        {
+            rule: 'minLength',
+            value: 3,
+        },
+        {
+            rule: 'maxLength',
+            value: 30,
+        },
+        {
+            rule: 'required',
+            value: true,
+            errorMessage: '!',
+        },
+    ])
+    .addField('#lastName', [
+        {
+            rule: 'minLength',
+            value: 3,
+        },
+        {
+            rule: 'maxLength',
+            value: 30,
+        },
+        {
+            rule: 'required',
+            value: true,
+            errorMessage: '!',
+        },
+    ])
+    .addField('#email', [
+        {
+            rule: 'required',
+            value: true,
+            errorMessage: '!',
+        },
+        {
+            rule: 'email',
+            value: true,
+            errorMessage: '!',
+        },
+    ])
+    .addField('#tel', [
+        {
+            rule: 'required',
+            value: true,
+            errorMessage: '!',
+        },
+        {
+            rule: 'function',
+            validator: function () {
+                const phone = telSelector.inputmask.unmaskedvalue();
+                return phone.length === 10;
+            },
+            errorMessage: '!',
+        },
+    ])
+    .addField('#password', [
+        {
+            rule: 'required',
+            value: true,
+            errorMessage: '!',
+        },
+    ])
+    .onSuccess((event) => {
+        console.log('Validation passes and form submitted', event);
+
+        let formData = new FormData(event.target);
+
+        console.log(...formData);
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    console.log('Sent');
+                }
+            }
+        };
+
+        xhr.open('POST', 'mail.php', true);
+        xhr.send(formData);
+
+        event.target.reset();
+    });

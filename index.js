7d621b184993dd0f7f663c200f2ad5e68c211350
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2023-03-12 11:48:04 +0200

    test

--- index.js
+++ index.js
@@ -65,8 +65,8 @@ app.post("/send_email", (req, response) => {
     });
 })
 
-// server.listen(port, function () {
-//     console.log("port: " + port)
-// })
+server.listen(port, function () {
+    console.log("port: " + port)
+})
 
 // ohfbxqcoqrhpvzmh
